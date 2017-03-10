4b2eeb27cd05ffcb6550ea89c7c7b45759a49889
Tran Truong <Tran Truong>
2017-03-10 10:23:25 +0700

    Add google map

--- resources/views/frontend/block/contact.blade.php
+++ resources/views/frontend/block/contact.blade.php
@@ -16,17 +16,15 @@
               <li class="email"> test@test.com</li>
             </ul>
         </div>
-        <div class="col-sm-3 twitter">
-            <h2>Twitter </h2>
-            <div id="twitter">
-            This is twitter
-            </div>
-        </div>
         <div class="col-sm-3 aboutus">
             <h2>About Us </h2>
             <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. <br>
               <br>
               t has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
+        </div>
+        <div class="col-sm-3 twitter">
+            <h2>Location Us</h2>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.9872403737327!2d105.852645814504!3d20.993148586017668!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ac12ee34ca35%3A0xa48f99dde993dc90!2zTmfDtSBH4buRYyDEkOG7gSwgSMOgIE7hu5lpLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1489115811613" width="270" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>
         </div>
     </div>
   </div>
